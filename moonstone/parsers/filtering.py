b97b6f2481fcbbdbd607592c43bf61108e94d53e
skennedy <sean.kennedy@pasteur.fr>
2019-11-12 16:02:04 +0100

    Starting adding logging to replace 'print' statements.
    Modified filtering.py only prior to creating a marge request.

--- moonstone/parsers/filtering.py
+++ moonstone/parsers/filtering.py
@@ -1,11 +1,16 @@
+import logging
+logging.basicConfig(filename='output/info.log', filemode='w', level=logging.DEBUG)
+
+
 class Filtering(object):
     def __init__(self, df, mean_filter):
         self.df = df
         self.mean_filter = mean_filter
 
     def by_mean(self):
-        print("Filtering option selected...filtering data by mean reads.")
+        logging.info('Started: Filtering by mean number of reads.')
         df_filtered = self.df[self.df.columns[self.df.mean() >= self.mean_filter]]
         num_vars = df_filtered.shape[1]
-        print(f'Retained {num_vars} taxa with at least {self.mean_filter} mean reads.')
+        logging.info('Retained %i taxa with at least %d mean reads.' % (num_vars, self.mean_filter))
+        logging.info('Completed: Filtering by mean number of reads. Have a nice day!')
         return df_filtered
