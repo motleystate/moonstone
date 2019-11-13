7afa281bf87c2845d973e3d5a5f2c2265a121b75
Mariela <mariela.furstenheim@pasteur.fr>
2019-11-13 10:46:25 +0100

    finish the tests and removed the extra properties

--- moonstone/normalization/DESeq2_normalization.py
+++ moonstone/normalization/DESeq2_normalization.py
@@ -6,47 +6,34 @@ class DESeq2_normalization:
     def __init__(self, df):
         self.df = df
 
-    @property
     def non_zero_df(self):
-        if getattr(self, "_non_zero_df", None) is None:
-            df_without_zero = self.df.copy().replace(0, np.nan).dropna().astype(int)
-            setattr(self, "_non_zero_df", df_without_zero)
-        return self._non_zero_df
+        """
+        This function removes lines with 0 reads
+        """
+        return self.df.copy().replace(0, np.nan).dropna().astype(int)
 
-    @property
     def log_df(self):
-        if getattr(self, "_log_df", None) is None:
-            log_non_zero_df = np.log(self.non_zero_df)
-            setattr(self, "_log_df", log_non_zero_df)
-        return self._log_df
+        return np.log(self.non_zero_df())
 
-    @property
     def mean_gen_values(self):
-        if getattr(self, "_mean_gen_values", None) is None:
-            Log_Mean = self.log_df.mean(axis=1)
-            setattr(self, "_mean_gen_values", Log_Mean)
-        return self._mean_gen_values
+        return self.log_df().mean(axis=1)
 
-    # Substracting the Log_mean to log original values
-    @property
     def subs_log_values(self):
-        if getattr(self, "_subs_log_values", None) is None:
-            Substracted_df = self.log_df.sub(self.mean_gen_values, axis='rows')
-            setattr(self, "_subs_log_values", Substracted_df)
-        return self._subs_log_values
+        """
+        Substracting the Log_mean to log original values
+        """
+        return self.log_df().sub(self.mean_gen_values(), axis='rows')
 
-    # Calculating the median per sample
-    @property
     def sample_log_median(self):
-        if getattr(self, "_sample_log_median", None) is None:
-            Median_df = self.subs_log_values.median()
-            setattr(self, "_sample_log_median", Median_df)
-        return self._sample_log_median
+        """
+        Calculating the median per sample
+        """
+        return self.subs_log_values().median()
 
     @property
     def scaling_factor(self):
         if getattr(self, "_scaling_factor", None) is None:
-            Scaling_factors = np.exp(self.sample_log_median)
+            Scaling_factors = np.exp(self.sample_log_median())
             setattr(self, "_scaling_factor", Scaling_factors)
         return self._scaling_factor
 
--- moonstone/normalization/testDESeq2_normalization.py
+++ moonstone/normalization/testDESeq2_normalization.py
@@ -9,12 +9,16 @@ from moonstone.normalization.DESeq2_normalization import (
 
 class TestSequence(TestCase):
 
-
     def setUp(self):
-        data = [[255,26,48,75],[366,46,78,0],[955,222,46,65],[89,54,145,29]]
+        data = [
+            [255, 26, 48, 75],
+            [366, 46, 78, 0],
+            [955, 222, 46, 65],
+            [89, 54, 145, 29]
+        ]
         column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
-        ind =["Gen_1", 'Gen_2', "Gen_3",'Gen_4']
-        self.dummy_df = pd.DataFrame(data, columns = column_names, index = ind)
+        ind = ["Gen_1", 'Gen_2', "Gen_3", 'Gen_4']
+        self.dummy_df = pd.DataFrame(data, columns=column_names, index=ind)
 
     def test_check_format(self):
         tested_object = DESeq2_normalization(self.dummy_df)
@@ -22,29 +26,68 @@ class TestSequence(TestCase):
 
     def test_non_zero_df(self):
         tested_object = DESeq2_normalization(self.dummy_df)
-        expected_result = pd.DataFrame([[255,26,48,75],[955,222,46,65],[89,54,145,29]],
-            columns=['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4'], index=["Gen_1", "Gen_3",'Gen_4'])
-        pd.testing.assert_frame_equal(tested_object.non_zero_df, expected_result)
+        data = [
+            [255, 26, 48, 75],
+            [955, 222, 46, 65],
+            [89, 54, 145, 29]
+        ]
+        column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        ind = ["Gen_1", "Gen_3", 'Gen_4']
+        expected_result = pd.DataFrame(data, columns=column_names, index=ind)
+        pd.testing.assert_frame_equal(tested_object.non_zero_df(), expected_result)
 
     def test_log_df(self):
         tested_object = DESeq2_normalization(self.dummy_df)
-        expected_result = pd.DataFrame([[5.541264, 3.258097, 3.871201, 4.317488],[6.861711, 5.402677, 3.828641, 4.174387],[4.488636, 3.988984, 4.976734, 3.367296]],
-            columns=['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4'], index=["Gen_1", "Gen_3",'Gen_4'])
-        pd.testing.assert_frame_equal(tested_object.log_df, expected_result)
+        data = [
+            [5.541264, 3.258097, 3.871201, 4.317488],
+            [6.861711, 5.402677, 3.828641, 4.174387],
+            [4.488636, 3.988984, 4.976734, 3.367296]
+        ]
+        column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        ind = ["Gen_1", "Gen_3", 'Gen_4']
+        expected_result = pd.DataFrame(data, columns=column_names, index=ind)
+        pd.testing.assert_frame_equal(tested_object.log_df(), expected_result)
 
     def test_mean_gen_values(self):
         tested_object = DESeq2_normalization(self.dummy_df)
-        expected_result = pd.Series([4.247012, 5.066854, 4.205412], index=["Gen_1", "Gen_3",'Gen_4'])
-        pd.testing.assert_series_equal(tested_object.mean_gen_values, expected_result)
+        expected_result = pd.Series([4.247012, 5.066854, 4.205412], index=["Gen_1", "Gen_3", 'Gen_4'])
+        pd.testing.assert_series_equal(tested_object.mean_gen_values(), expected_result)
 
     def test_subs_log_values(self):
         tested_object = DESeq2_normalization(self.dummy_df)
-        expected_result = pd.DataFrame([[1.294251, -0.988916, -0.375811, 0.070476],[1.794857, 0.335823, -1.238213, -0.892467],[0.283224, -0.216428, 0.771321, -0.838117]],
-            columns=['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4'], index=["Gen_1", "Gen_3",'Gen_4'])
-        pd.testing.assert_frame_equal(tested_object.subs_log_values, expected_result)
+        data = [
+            [1.294251, -0.988916, -0.375811, 0.070476],
+            [1.794857, 0.335823, -1.238213, -0.892467],
+            [0.283224, -0.216428, 0.771321, -0.838117]
+        ]
+        column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        ind = ["Gen_1", "Gen_3", 'Gen_4']
+        expected_result = pd.DataFrame(data, columns=column_names, index=ind)
+        pd.testing.assert_frame_equal(tested_object.subs_log_values(), expected_result)
 
-    # countinue here
     def test_sample_log_median(self):
         tested_object = DESeq2_normalization(self.dummy_df)
-        expected_result = pd.Series([4.247012, 5.066854, 4.205412], index=["Gen_1", "Gen_3",'Gen_4'])
-        pd.testing.assert_series_equal(tested_object.sample_log_median, expected_result)
+        data = [1.294251, -0.216428, -0.375811, -0.838117]
+        ind = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        expected_result = pd.Series(data, index=ind)
+        pd.testing.assert_series_equal(tested_object.sample_log_median(), expected_result)
+
+    def test_scaling_factor(self):
+        tested_object = DESeq2_normalization(self.dummy_df)
+        data = [3.648263, 0.805390, 0.686732, 0.432524]
+        ind = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        expected_result = pd.Series(data, index=ind)
+        pd.testing.assert_series_equal(tested_object.scaling_factor, expected_result)
+
+    def test_normalized_df(self):
+        tested_object = DESeq2_normalization(self.dummy_df)
+        data = [
+            [69.896271,	32.282490, 69.896271, 173.400644],
+            [100.321707, 57.115175, 113.581441, 0.000000],
+            [261.768388, 275.642802, 66.983926, 150.280558],
+            [24.395169, 67.048249, 211.144986, 67.048249]
+        ]
+        column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        ind = ["Gen_1", "Gen_2", "Gen_3", 'Gen_4']
+        expected_result = pd.DataFrame(data, columns=column_names, index=ind)
+        pd.testing.assert_frame_equal(tested_object.normalized_df, expected_result)
