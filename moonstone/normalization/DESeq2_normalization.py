9fe5f1280dbb803ed4f23fc2155b8c1b1f6cec61
Mariela <mariela.furstenheim@pasteur.fr>
2019-11-15 14:47:59 +0100

    Missing how to change the base exponent in CSF

--- moonstone/normalization/DESeq2_normalization.py
+++ moonstone/normalization/DESeq2_normalization.py
@@ -8,17 +8,18 @@ class DESeq2_normalization:
 
     def non_zero_df(self, df):
         """
-        This function removes lines with 0 reads
+        This function removes rows with 0 reads
         """
         return df.replace(0, np.nan).dropna().astype(int)
 
-    # change the log_nb
     def log_df(self, df, log_nb=None):
-        return np.log(df)
+        if log_nb is None:
+            return np.log(df)
+        else:
+            return np.log(df) / np.log(log_nb)
 
     def remove_zero_and_log(self, df):
         return self.log_df(self.non_zero_df(df))
-
 
     def calculating_and_substracting_mean_row(self, df):
         """
@@ -26,24 +27,19 @@ class DESeq2_normalization:
         """
         return df.sub(df.mean(axis=1), axis='rows')
 
-    def column_median(self, df):
-        """
-        Calculating the median per sample
-        """
-        return df.median()
-
+    # Only missing how to change the base_exponent
     @property
-    def scaling_factor(self):
-        if getattr(self, "_scaling_factor", None) is None:
+    def compute_scaling_factor(self):
+        if getattr(self, "_compute_scaling_factor", None) is None:
             non_zero_log_df = self.remove_zero_and_log(self.df)
             substracted_mean_df = self.calculating_and_substracting_mean_row(non_zero_log_df)
             Scaling_factors = np.exp(substracted_mean_df.median())
-            setattr(self, "_scaling_factor", Scaling_factors)
-        return self._scaling_factor
+            setattr(self, "_compute_scaling_factor", Scaling_factors)
+        return self._compute_scaling_factor
 
     @property
     def normalized_df(self):
         if getattr(self, "_normalized_df", None) is None:
-            final_df = self.df.div(self.scaling_factor)
+            final_df = self.df.div(self.compute_scaling_factor)
             setattr(self, "_normalized_df", final_df)
         return self._normalized_df
--- moonstone/normalization/testDESeq2_normalization.py
+++ moonstone/normalization/testDESeq2_normalization.py
@@ -58,6 +58,29 @@ class TestSequence(TestCase):
 
         pd.testing.assert_frame_equal(tested_object.log_df(input_df), expected_result)
 
+    def test_log_base_n_df(self):
+        input_data = [
+            [255, 26, 48, 75],
+            [955, 222, 46, 65],
+            [89, 54, 145, 29]
+        ]
+        input_column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        input_ind = ["Gen_1", "Gen_3", 'Gen_4']
+        input_df = pd.DataFrame(input_data, columns=input_column_names, index=input_ind)
+        tested_object = DESeq2_normalization(self.dummy_df)
+
+        data = [
+            [2.406540, 1.414973, 1.681241, 1.875061],
+            [2.980003, 2.346353, 1.662758, 1.812913],
+            [1.949390, 1.732394, 2.161368, 1.462398]
+        ]
+        column_names = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
+        ind = ["Gen_1", "Gen_3", 'Gen_4']
+        expected_result = pd.DataFrame(data, columns=column_names, index=ind)
+
+        pd.testing.assert_frame_equal(tested_object.log_df(input_df, 10),
+                                      expected_result)
+
     def test_calculating_and_substracting_mean_row(self):
         input_data = [
             [5.541264, 3.258097, 3.871201, 4.317488],
@@ -77,22 +100,15 @@ class TestSequence(TestCase):
         ind = ["Gen_1", "Gen_3", 'Gen_4']
         expected_result = pd.DataFrame(data, columns=column_names, index=ind)
 
-        pd.testing.assert_frame_equal(tested_object.calculating_and_substracting_mean_row(input_df).round(6), expected_result)
-
-    #not changed yet
-    def test_sample_log_median(self):
-        tested_object = DESeq2_normalization(self.dummy_df)
-        data = [1.294251, -0.216428, -0.375811, -0.838117]
-        ind = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
-        expected_result = pd.Series(data, index=ind)
-        pd.testing.assert_series_equal(tested_object.sample_log_median(), expected_result)
+        pd.testing.assert_frame_equal(tested_object.calculating_and_substracting_mean_row(input_df).round(6),
+                                      expected_result)
 
     def test_scaling_factor(self):
         tested_object = DESeq2_normalization(self.dummy_df)
         data = [3.648263, 0.805390, 0.686732, 0.432524]
         ind = ['Sample_1', 'Sample_2', 'Sample_3', 'Sample_4']
         expected_result = pd.Series(data, index=ind)
-        pd.testing.assert_series_equal(tested_object.scaling_factor, expected_result)
+        pd.testing.assert_series_equal(tested_object.compute_scaling_factor, expected_result)
 
     def test_normalized_df(self):
         tested_object = DESeq2_normalization(self.dummy_df)
