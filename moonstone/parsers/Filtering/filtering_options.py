8e4c056d8d75182f9ff6187c6e5c86ba38d7713c
Mariela F <mariela.furstenheim@pasteur.fr>
2019-12-11 10:00:53 +0100

    changing part of the comments

--- moonstone/parsers/Filtering/filtering_options.py
+++ moonstone/parsers/Filtering/filtering_options.py
@@ -5,10 +5,17 @@ class FilteringOptions:
      - Some rows contain non relevant information
 
     In additon, it can be used to only get the desired rows to study.
+    
+    This class assumes that the samples are placed on the different 
+    columns and features (or taxa) are specified on the rows. Ex:
+    samples  1  2  3  4
+    feature1 x  y  t  g
+    feature2 f  h  j  k
+    feature3 l  m  o  p
     """
     def __init__(self, dataframe):
         self.counts_df = dataframe
-        self.raw_dataframe = dataframe
+        self.raw_df = dataframe
         self.steps = []
 
     def remove_data_without_metadata(self, metadata_df):
@@ -16,12 +23,12 @@ class FilteringOptions:
         self.steps.append('remove_data_without_metadata')
         return self.counts_df
 
-    def remove_rows_wihtout_relevant_info(self, name_of_rows, level_name_provided):
+    def remove_rows_without_relevant_info(self, name_of_rows, level_name_provided):
         self.counts_df = self.counts_df.drop(index=name_of_rows, level=level_name_provided)
         self.steps.append('remove_rows_wihtout_relevant_info')
         return self.counts_df
 
-    def selecting_certain_rows(self, desired_row_series, level_to_check):
+    def selecting_rows(self, desired_row_series, level_to_check):
         self.counts_df = self.counts_df[self.counts_df.index.get_level_values(level_to_check).isin(desired_row_series)]
         self.steps.append('selecting_certain_rows')
         return self.counts_df
