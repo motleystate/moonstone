d365adc6776343c71ab3693ad0c8fe1211ddb053
Kenzo-Hugo Hillion <kenzo-hugo.hillion1@pasteur.fr>
2020-02-18 15:29:25 +0100

    add test for t__ example

--- tests/parsers/counts/taxonomy/metaphlan2/test_metaphlan2.py
+++ tests/parsers/counts/taxonomy/metaphlan2/test_metaphlan2.py
@@ -62,3 +62,26 @@ class TestMetaphlan2Parser(TestCase):
         )
         expected_df = expected_df.set_index(['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species'])
         pd.testing.assert_frame_equal(self.meta2parser.dataframe, expected_df)
+
+    def test_to_dataframe_with_t__(self):
+        """
+        Test based on input_with_t__.tsv file, meaning it has annotation until the t__ level
+        """
+        input_path = os.path.join(os.path.dirname(__file__), 'input_with_t__.tsv')
+        self.meta2parser = Metaphlan2Parser(input_path)
+        expected_df = pd.DataFrame(
+            [
+                ['Bacteria', 'Actinobacteria', 'Actinobacteria', 'Actinomycetales', 'Actinomycetaceae', 'Actinomycetaceae (family)', 'Actinomycetaceae (family)', 'Actinomycetaceae (family)', 5.5, 6.0],  # noqa
+                ['Bacteria', 'Actinobacteria', 'Actinobacteria', 'Actinomycetales', 'Actinomycetaceae', 'Actinobaculum', 'Actinobaculum (genus)', 'Actinobaculum (genus)', 4.3, 2.1],   # noqa
+                ['Bacteria', 'Actinobacteria', 'Actinobacteria', 'Actinomycetales', 'Actinomycetaceae', 'Actinobaculum', 'Actinobaculum_massiliense', 'GCF_000315465', 1.0, 2.0]   # noqa
+            ],
+            columns=[
+                'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'sTrain', 'SAMPLE_1', 'SAMPLE_2'
+            ]
+        )
+        expected_df = expected_df.set_index(
+            [
+                'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'sTrain'
+            ]
+        )
+        pd.testing.assert_frame_equal(self.meta2parser.dataframe, expected_df)
