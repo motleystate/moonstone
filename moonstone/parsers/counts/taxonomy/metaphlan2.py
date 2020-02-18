8b72428abc0fc58535cd1ced4ac5fb39f7f6de4f
Kenzo-Hugo Hillion <kenzo-hugo.hillion1@pasteur.fr>
2020-02-18 14:08:03 +0100

    fix tests

--- moonstone/parsers/counts/taxonomy/metaphlan2.py
+++ moonstone/parsers/counts/taxonomy/metaphlan2.py
@@ -42,7 +42,8 @@ class Metaphlan2Parser(BaseParser):
                 return string.split('__')[-1]
 
         taxa_columns = df[self.taxa_column].str.split("|", expand=True)
-        taxa_columns.columns = self.taxonomical_names[:len(taxa_columns.columns)]
+        self._rank_level = len(taxa_columns.columns)
+        taxa_columns.columns = self.taxonomical_names[:self._rank_level]
         taxa_columns = taxa_columns.applymap(lambda x: remove_taxo_prefix(x))
         taxa_columns = self._fill_none(taxa_columns)
         return pd.concat([self._fill_none(taxa_columns), df.drop(self.taxa_column, axis=1)], axis=1)
@@ -50,5 +51,5 @@ class Metaphlan2Parser(BaseParser):
     def to_dataframe(self):
         df = super().to_dataframe()
         df = self.split_taxa_fill_none(df)
-        df = df.set_index(self.taxonomical_names)
+        df = df.set_index(self.taxonomical_names[:self._rank_level])
         return df
