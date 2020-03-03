9915ed564ba60cecac93da3c1b6ebd8bfb0c18ad
skennedy8 <seanpkennedy@gmail.com>
2020-03-03 14:25:18 +0100

    Created new test module for scaler module

---
+++ tests/normalization/processed/test_standard_scaler.py
@@ -0,0 +1,31 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_standard_scaler(self):
+        self.assertEqual(True, False)
+
+        df = pd.DataFrame(
+            [
+                [1., -1., 2.],
+                [2., 0., 0.],
+                [0., 1., -1]
+            ],
+            columns=['A', 'B', 'C']
+        )
+
+        df_expected = pd.DataFrame(
+            [
+                [0., -1.224745, 1.336306],
+                [1.224745, 0., -0.267261],
+                [-1.224745, 1.224745, -1.069045],
+            ],
+            columns=['A', 'B', 'C']
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
