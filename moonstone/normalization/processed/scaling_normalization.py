Corrected generic naming of scaling test issue. Changed expected type to NumPy array in scalTestStandardScaler

moonstone/normalization/processed/scaling_normalization.py
@@ -19,11 +19,14 @@ class StandardScaler(BaseScaling):
     def __init__(self, raw_x):
         super().__init__(raw_x)
         logger.info(f'Starting instance of {__class__.__name__} in {__name__}.')
-        self.raw_x = raw_x
         if not isinstance(self.raw_x, np.ndarray):
             raise ValueError('A NumPy array is required for normalization. Got {}'.format(type(self.raw_x)))
 
-    def scaler(self):
+    def scaler(self) -> np.ndarray:
+        """
+        Takes a NumPy array of the independent variables, or features, as 'x' for ML training.
+        """
+
         logger.info("Counts standardized by Standard Scalar. Check Mean ~0.0 and similar Variances:")
         scaled_x = preprocessing.scale(self.raw_x, with_mean=True, with_std=True)
         stats.normalized_stats(scaled_x)

tests/normalization/processed/test_scaling_normalization.py
@@ -1,30 +1,28 @@
 import unittest
-import pandas as pd
+import numpy as np
 
 from moonstone.normalization.processed.scaling_normalization import StandardScaler
 
 
-class MyTestCase(unittest.TestCase):
+class TestStandardScaler(unittest.TestCase):
 
     def test_standard_scaler(self):
-        df = pd.DataFrame(
+        raw = np.array(
             [
                 [1., -1., 2.],
                 [2., 0., 0.],
                 [0., 1., -1]
-            ],
-            columns=['A', 'B', 'C']
+            ]
         )
-        df_expected = pd.DataFrame(
+        expected = np.array(
             [
                 [0., -1.224745, 1.336306],
                 [1.224745, 0., -0.267261],
                 [-1.224745, 1.224745, -1.069045],
-            ],
-            columns=['A', 'B', 'C']
+            ]
         )
-        scaler = StandardScaler(df).scale_x
-        pd.testing.assert_frame_equal(scaler, df_expected)
+        scaler = StandardScaler.scaler(raw)
+        np.array_equal(scaler, expected)
 
 
 if __name__ == '__main__':