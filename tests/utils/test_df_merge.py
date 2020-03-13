6804f9757b579a05a2659d4c5fdcd9045b1127cc
skennedy8 <seanpkennedy@gmail.com>
2020-03-13 15:42:27 +0100

    finished test_df_merge. Test passes.

--- tests/utils/test_df_merge.py
+++ tests/utils/test_df_merge.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 import pandas as pd
 
-from moonstone.utils import df_merge
+from moonstone.utils.df_merge import MergeDF
 
 
 class TestMergeDF(TestCase):
@@ -29,3 +29,16 @@ class TestMergeDF(TestCase):
             index=[1, 2, 3, 4]  # index dtype='int64'
         )
 
+        df_expected = pd.DataFrame(
+            [
+                ['M', 23, 7, 44, 0, 101],
+                ['F', 15, 4, 76, 3, 107],
+                ['F', 20, 0, 22, 0, 101],
+                ['F', 31, 4, 50, 0, 99]
+            ],
+            columns=['sex', 'item_1', 'item_2', 'item_3', 'item_4', 'item_5'],
+            index=[1, 2, 3, 4]  # index dtype='int64'
+        )
+
+        merged_df = MergeDF(d1, d2, 'sex').merged_df
+        pd.testing.assert_frame_equal(merged_df, df_expected)
