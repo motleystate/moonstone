aee704eaa8d643d7981cdaa38dcb300d741d2399
Agnes BAUD <agnes.baud@pasteur.fr>
2020-09-15 15:21:27 +0200

    slight refactoring

--- moonstone/analysis/diversity/alpha.py
+++ moonstone/analysis/diversity/alpha.py
@@ -3,6 +3,7 @@ from abc import ABC
 from typing import Union, Optional
 
 import pandas as pd
+import re
 import skbio
 
 from moonstone.core.module_base import BaseModule, BaseDF
@@ -20,30 +21,30 @@ class BaseAnalysis(BaseModule, BaseDF, ABC):
         super().__init__(dataframe)
 
 
-class ShannonIndex(BaseModule, BaseDF, ABC):
+class AlphaDiversity(BaseModule, BaseDF, ABC):
 
     def __init__(self, dataframe: Union[pd.Series, pd.DataFrame]):
         super().__init__(dataframe)
+        self.index_name = " ".join(re.findall('[A-Z][^A-Z]*', self.__class__.__name__)).lower()
 
-    def compute_shannon_diversity(self, base: Union[int, float] = 2):    # compute_shannon_diversity
-        # steps to compute the index
-        Seriesdic = {}
-        for i in self.df.columns:
-            Seriesdic[i] = skbio.diversity.alpha.shannon(self.df[i], base)
-        return pd.Series(Seriesdic)
+    def compute_alpha_diversity(self):
+        """
+        method that compute the alpha diversity
+        """
+        return "choose index computation method"
 
     @property
-    def shannon_indexes(self):
-        # call compute_shannon_diversity and store into self._shannon_indexes
-        if getattr(self, '_shannon_indexes', None) is None:
-            self._shannon_indexes = self.compute_shannon_diversity()
-        return self._shannon_indexes
+    def alpha_diversity_indexes(self):
+        # call compute_alpha_diversity and store into self._alpha_indexes
+        if getattr(self, '_alpha_diversity_indexes', None) is None:
+            self._alpha_diversity_indexes = self.compute_alpha_diversity()
+        return self._alpha_diversity_indexes
 
     def visualize(self, bins_size: Union[int, float] = 0.1, plotting_options: dict = None,
                   show: Optional[bool] = True, output_file: Optional[str] = False):
 
-        title = "shannon indexes (alpha diversity) distribution across the samples"
-        xlabel = "shannon index"
+        title = self.index_name+" (alpha diversity) distribution across the samples"
+        xlabel = self.index_name
         ylabel = "number of samples"
 
         if plotting_options is None:
@@ -55,10 +56,24 @@ class ShannonIndex(BaseModule, BaseDF, ABC):
                                                                       title,
                                                                       xlabel, ylabel)
 
-        hist_fig = Histogram(self._shannon_indexes)
+        hist_fig = Histogram(self._alpha_diversity_indexes)
         hist_fig.plot_one_graph(
             bins_size,
             plotting_options=plotting_options,
             show=show,
             output_file=output_file,
             )
+
+
+class ShannonIndex(AlphaDiversity):
+
+    def compute_alpha_diversity(self, base: Union[int, float] = 2):    # compute_shannon_diversity
+        """
+        :param base: logarithm base chosen (NB : for ln, base=math.exp(1))
+        """
+        # steps to compute the index
+        Seriesdic = {}
+        for i in self.df.columns:
+            Seriesdic[i] = skbio.diversity.alpha.shannon(self.df[i], base)
+        return pd.Series(Seriesdic)
+
--- tests/analysis/diversity/test_alpha.py
+++ tests/analysis/diversity/test_alpha.py
@@ -11,19 +11,18 @@ class TestShannonIndex(TestCase):
 
         tested_object = pd.DataFrame.from_dict(
             {
-                'Items1': [4,4,0,0,4],
-                'Items2': [1,0,2,0,5],
-                'Items3': [0,0,0,1,4],
-                'Items4': [0,3,0,0,4]
+                'species1': [4,4,0,0,4],
+                'species2': [1,0,2,0,5],
+                'species3': [0,0,0,1,4],
+                'species4': [0,3,0,0,4]
             },
             orient='index', columns=['sample1', 'sample2', 'sample3', 'sample4', 'sample5'])
         tested_object_instance = ShannonIndex(tested_object)
-        # expected_object = pd.Series({
-        #     'sample1': 0.721928,
-        #     'sample2': 0.985228
-        #     'sample3': -0.000000
-        #     'sample4': -0.000000
-        #     'sample5': 1.992778
-        # })
-        tested_object_instance.compute_shannon_diversity()
-        # TO FINISH
+        expected_object = pd.Series({
+            'sample1': 0.721928,
+            'sample2': 0.985228,
+            'sample3': -0.000000,
+            'sample4': -0.000000,
+            'sample5': 1.992778
+        })
+        pd.testing.assert_series_equal(tested_object_instance.compute_alpha_diversity(), expected_object)
