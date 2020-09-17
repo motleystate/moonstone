914d65b23088fa9188729548e8dc8c2826002248
skennedy8 <seanpkennedy@gmail.com>
2020-09-17 17:31:58 +0200

    fixed misplaced return in base module. Fixed variable assignment to accept tuple from downsize_pair function.

--- moonstone/normalization/reads/base.py
+++ moonstone/normalization/reads/base.py
@@ -20,14 +20,14 @@ class BaseDownsizing:
     @property
     def downsized_pair(self):
         if getattr(self, "._downsized_f", None) is None:
-            self._downsized_f = self.downsize_pair()
+            self._downsized_f, self_downsized_r = self.downsize_pair()
             self.f = self._downsized_f
             self.r = self._downsized_r
-            return self._downsized_f, self._downsized_r
+        return self._downsized_f, self._downsized_r
 
     @property
     def downsized_single(self):
         if getattr(self, "._downsized_f", None) is None:
             self._downsized_f = self.downsize_single()
             self.f = self._downsized_f
-            return self._downsized_f
+        return self._downsized_f
