bf68f6f262ff362b3f757c9f7f0f42845213045a
Agnes BAUD <agnes.baud@pasteur.fr>
2020-10-26 10:38:04 +0100

    warnings empty group

--- moonstone/analysis/statistical_test.py
+++ moonstone/analysis/statistical_test.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 def mann_whitney_u_group(series, group_series, alternative='two-sided'):
     groups = list(group_series.unique())
@@ -9,12 +12,14 @@ def mann_whitney_u_group(series, group_series, alternative='two-sided'):
 
     tab = [[np.nan] * len(groups) for _ in range(len(groups))]
     for i in range(len(groups)):
+        groupi = series[group_series[group_series == groups[i]].index].dropna()
+        if groupi.size == 0:
+            logger.warning(f'No samples from group {groups[i]} in data.')
         for j in range(i+1, len(groups)):
-            if i != j:
-                pval = stats.mannwhitneyu(series[group_series[group_series == groups[i]].index],
-                                          series[group_series[group_series == groups[j]].index],
-                                          alternative=alternative)[1]
-                tab[i][j] = pval
-                tab[j][i] = pval
+            pval = stats.mannwhitneyu(groupi,
+                                      series[group_series[group_series == groups[j]].index].dropna(),
+                                      alternative=alternative)[1]
+            tab[i][j] = pval
+            tab[j][i] = pval
     mann_whitney_u_df = pd.DataFrame(tab, index=groups, columns=groups)
     return mann_whitney_u_df
--- tests/analysis/test_statistical_test.py
+++ tests/analysis/test_statistical_test.py
@@ -42,3 +42,32 @@ class TestShannonIndex(TestCase):
 
         matrix = mann_whitney_u_group(test_df, metadata_df['group'])
         pd.testing.assert_frame_equal(matrix, expected_df, check_dtype=False)
+
+    def test_mann_whitney_u_group_wrong_metadata(self):
+        test_df = pd.Series({
+            'sample1': 0.72,
+            'sample2': 0.98,
+            'sample3': 0.00,
+            'sample4': 0.00,
+            'sample5': 1.99
+        })
+        metadata_df = pd.DataFrame(
+            [
+                ['F', 2],
+                ['M', 1]
+            ],
+            columns=['sex', 'group'],
+            index=['sample6', 'sample7'],
+        )
+
+        expected_df = pd.DataFrame(
+            [
+                [np.nan, 0],
+                [0, np.nan]
+            ],
+            columns=[1, 2],
+            index=[1, 2]
+        )
+
+        matrix = mann_whitney_u_group(test_df, metadata_df['group'])
+        pd.testing.assert_frame_equal(matrix, expected_df, check_dtype=False)
