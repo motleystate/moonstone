2d1d99f65740934f202b02a7a3bff3e131bb314d
Kenzo-Hugo Hillion <hillion.kenzo@posteo.net>
2020-12-31 16:14:03 +0100

    add method to visualize categories of metadata

--- moonstone/parsers/metadata.py
+++ moonstone/parsers/metadata.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from typing import Dict, List
 
 from pandas import DataFrame
+import plotly.graph_objects as go
 
 from moonstone.analysis.columns_statistics import DataframeStatistics
 from moonstone.parsers.transform.cleaning import DataFrameCleaner
@@ -13,6 +14,15 @@ class MetadataParser(BaseParser):
     """
     Parse metadata file and allows to apply transformations on them (cleaning...).
     """
+
+    DEFAULT_COLORSCALE = [
+        [0, "rgb(166,206,227)"],
+        [0.25, "rgb(31,120,180)"],
+        [0.45, "rgb(178,223,138)"],
+        [0.65, "rgb(51,160,44)"],
+        [0.85, "rgb(251,154,153)"],
+        [1, "rgb(227,26,28)"]
+    ]
 
     def __init__(self, *args, index_col: str = 'sample', cleaning_operations: dict = None, **kwargs):
         """
@@ -47,6 +57,51 @@ class MetadataParser(BaseParser):
         :return: list of dict containing statistics about each column
         """
         return DataframeStatistics(self.dataframe).get_stats()
+
+    def _get_dimensions(self, categories):
+        dimensions = []
+        for cat in categories:
+            dimensions.append(
+                go.parcats.Dimension(
+                    values=self.dataframe[cat],
+                    categoryorder='category ascending', label=cat
+                )
+            )
+        return dimensions
+
+    def _get_color(self, color_by: str):
+        cpt = 1
+        color_dict = {}
+        for i in self.dataframe[color_by].unique():
+            color_dict[i] = cpt
+            cpt += 1
+        return self.dataframe[color_by].apply(lambda x: color_dict.get(x, 0))
+
+    def visualize_categories(self, categories: list, color_by: str, colorscale: list = None):
+        """
+        Visualize category metadata.
+
+        :param categories: list of column to display
+        :param color_by: perform coloration on the given category
+        """
+        if colorscale is None:
+            colorscale = self.DEFAULT_COLORSCALE
+        dimensions = self._get_dimensions(categories)
+        color = self._get_color(color_by)
+
+        fig = go.Figure(
+            data=[
+                go.Parcats(
+                    dimensions=dimensions,
+                    line={'color': color, 'colorscale': colorscale},
+                    hoverinfo='count',
+                    labelfont={'size': 18, 'family': 'Times'},
+                    tickfont={'size': 16, 'family': 'Times'},
+                    arrangement='freeform'
+                )
+            ]
+        )
+        fig.show()
 
 
 class YAMLBasedMetadataParser:
