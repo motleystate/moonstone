c513aaa731a73f541bee423cd313649458acb927
Agnes BAUD <agnes.baud@pasteur.fr>
2023-10-12 14:08:17 +0200

    updating scipy (everywhere) and plotly (in setup)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         'pandas==2.0.2',
         'matplotlib==3.3.0',
-        'plotly==5.6.0',
+        'plotly==5.17.0',
         'statsmodels==0.13.0',
         'python-slugify==4.0.1',
         'pyaml==20.4.0',
@@ -20,7 +20,7 @@ setup(
         'scikit-learn==1.3.1',
         'hdmedians==0.14.2',
         'cython==0.29.21',
-        'scipy==1.5.2'
+        'scipy==1.9.0'
     ],
     packages=find_packages(),
     entry_points={'console_scripts': ['moonstone=moonstone.main:run']},
