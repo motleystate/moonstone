1d60f1b239bab2fa77db510e5888dc947333b5a9
Agnes Baud <agnesbaud@hotmail.fr>
2025-02-05 19:33:54 +0100

    more unique symbols + more  tests

--- moonstone/plot/graphs/scatter.py
+++ moonstone/plot/graphs/scatter.py
@@ -2,6 +2,7 @@ import logging
 from typing import Union
 
 import plotly.graph_objects as go
+from plotly.validators.scatter.marker import SymbolValidator
 
 from moonstone.plot.graphs.base import BaseGraph, GroupBaseGraph
 
@@ -60,24 +61,86 @@ class GroupScatterGraph(GroupBaseGraph):
             marker=marker,
         )
 
-    def _symbol_scheme(self, groups: list):
-        list_fill_pattern = ["", "1", "3"]     # 2 is filled with a dot inside ("-dot")
-        # but it is not very clear
-        list_shape = [
-            "00", "01", "02", "03", "04", "05", "06", "17",
-            # add some more in case of more than 24 groups
-        ]
+    @property
+    def dictionary_marker_symbols(self):
+        """
+        All the symbols accepted by scatter as integer.
+        """
+        if getattr(self, '_dictionary_marker_symbols', None) is None:
+            raw_symbols = SymbolValidator().values
+            self._dictionary_marker_symbols = {}
+            for i in range(0, len(raw_symbols), 3):
+                self._dictionary_marker_symbols[raw_symbols[i+2]] = raw_symbols[i]
+        return self._dictionary_marker_symbols
 
-        # if I ever bring myself to class more shape, I'll uncomment below:
-        # to get my 8 fave shape first
-        # if len(groups) <= 24:
-        #   list_shape = list_shape[:8]
+    def _translating_forced_symbols(self, forced_symbols: Union[list, set]) -> set:
+        translated_symbols = []
+        txt_symbols = []
+        for x in forced_symbols:
+            if type(x) is int:
+                translated_symbols += [x]
+            elif x.isnumeric():
+                translated_symbols += [int(x)]
+            else:
+                txt_symbols += [x]
 
-        list_symbols = [int(prefix + suffix) for prefix in list_fill_pattern for suffix in list_shape]
-        if len(groups) <= 24:
-            return dict(zip(groups, list_symbols))
+        if len(txt_symbols) > 0:
+            for i in txt_symbols:
+                if i in self.dictionary_marker_symbols.keys():
+                    translated_symbols += [self.dictionary_marker_symbols[i]]
+                    # txt_symbols.remove(i)
+                else:
+                    # raise error
+                    error_message = f"Invalid symbol values: {txt_symbols}.\n\
+Accepted values: {SymbolValidator().values}"
+                    raise ValueError(error_message)
+        return set(translated_symbols)
+
+    def _symbol_scheme(self, groups: list, symbols: dict = None):
+        """
+        Generate a dictionary with the different groups as key and an int corresponding to a symbol (following
+        go.Marker's definition) as value.
+        If number of different groups > 24, then symbols are repeated.
+
+        Args:
+            metadata: Can take DataFrame or Series, then all different values are listed and assigned a color.
+                Or can take a list/array, then all elements of the list are assigned a color.
+                Finally, metadata can be an integer which act like a list ranging from 0 to that integer (excluded),
+                and each integer is assigned a color.
+            symbols: Dictionary that dictate a particular symbol to a particular group.
+        """
+        if len(groups) > 162:
+            list_symbols = list(self.dictionary_marker_symbols.values())
+            dic_symbols = dict(zip(groups, list_symbols * (int(len(groups) / len(list_symbols)) + 1)))
         else:
-            return dict(zip(groups, list_symbols * (int(len(groups) / len(list_symbols)) + 1)))
+            if len(groups) <= 72:
+                list_fill_pattern = ["", "1", "3"]     # 2 is filled with a dot inside ("-dot")
+                # but it is not very clear
+                list_shape = [
+                    "00", "01", "02", "03", "04", "05", "06", "17", "13", "18", "07", "08", "14", "23", "24", "19",
+                    "20", "21", "22", "16", "09", "10", "11", "12", "15",
+                ]
+                n = -(-len(groups) // 3)
+                if n <= 8:
+                    n = 8
+
+                list_symbols = [int(prefix + suffix) for prefix in list_fill_pattern for suffix in list_shape[:n]]
+            else:
+                list_symbols = self.dictionary_marker_symbols.values()
+
+            if symbols is not None:
+                to_remove = self._translating_forced_symbols(set(symbols.values()))
+
+                # for reusability, we can't use set().difference()
+                list_symbols = [x for x in list_symbols if x not in to_remove]
+                skeys = set(symbols.keys())
+                groups = [x for x in groups if x not in skeys]
+            dic_symbols = dict(zip(groups, list_symbols))
+
+        if symbols is not None:
+            dic_symbols.update(**symbols)
+
+        return dic_symbols
 
     def plot_one_graph(
         self,
@@ -117,7 +180,7 @@ class GroupScatterGraph(GroupBaseGraph):
                         self._gen_fig_trace(
                             filtered_df[first_col],
                             filtered_df[second_col],
-                            str(gp1),
+                            str(gp1)+" - "+str(gp2),
                             filtered_df.index,
                             self._get_group_color(gp1, colors),
                             marker=dict(symbol=dic_symbol[gp2]),
