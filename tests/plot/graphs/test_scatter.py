675ed35efbba4ae3834792cd46dc21fa2cc4be00
Agnes Baud <agnesbaud@hotmail.fr>
2025-02-07 14:52:08 +0100

    group_col2 for scatter3d + more tests

--- tests/plot/graphs/test_scatter.py
+++ tests/plot/graphs/test_scatter.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-from moonstone.plot.graphs.scatter import GroupScatterGraph
+from moonstone.plot.graphs.scatter import GroupScatterGraph, GroupScatter3DGraph
 
 
 class TestGroupScatterGraph(TestCase):
@@ -21,6 +21,7 @@ class TestGroupScatterGraph(TestCase):
         self.ins = GroupScatterGraph(self.test_df)
 
     def test_plot_one_graph_1groupcol(self):
+        # testing that it runs without error and checking a few variables
         tested_fig = self.ins.plot_one_graph(
             "var1", "var2", "group",
             show=False, plotting_options={"layout": {"height": 500, "width": 500}}
@@ -36,31 +37,32 @@ class TestGroupScatterGraph(TestCase):
         self.assertEqual(tested_fig["data"][2]["marker"]["color"], "#68ace8")
 
     def test_plot_one_graph_2groupcol(self):
+        # testing that it runs without error and checking a few variables
         tested_fig = self.ins.plot_one_graph(
             "var1", "var2", "group",
             group_col2="smoking", show=False
         )
         self.assertEqual(tested_fig["data"][0]["marker"], go.scatter.Marker({'color': '#A63A50', 'symbol': 0}))
-        self.assertEqual(tested_fig["data"][1]["marker"], go.scatter.Marker({'color': '#FFBF00', 'symbol': 0}))
-        self.assertEqual(tested_fig["data"][2]["marker"], go.scatter.Marker({'color': '#68ace8', 'symbol': 0}))
-        self.assertEqual(tested_fig["data"][3]["marker"], go.scatter.Marker({'color': '#A63A50', 'symbol': 1}))
+        self.assertEqual(tested_fig["data"][1]["marker"], go.scatter.Marker({'color': '#68ace8', 'symbol': 0}))
+        self.assertEqual(tested_fig["data"][2]["marker"], go.scatter.Marker({'color': '#A63A50', 'symbol': 1}))
+        self.assertEqual(tested_fig["data"][3]["marker"], go.scatter.Marker({'color': '#FFBF00', 'symbol': 1}))
 
     def test_dictionary_marker_symbols(self):
         expected_object = {
-            "circle": 0, "circle-open": 100, "circle-dot": 200, "circle-open-dot": 300, "square": 1,"square-open": 101,
-            "square-dot": 201, "square-open-dot": 301, "diamond": 2, "diamond-open": 102, "diamond-dot": 202,
-            "diamond-open-dot": 302, "cross": 3, "cross-open": 103, "cross-dot": 203, "cross-open-dot": 303, "x": 4,
-            "x-open": 104, "x-dot": 204, "x-open-dot": 304, "triangle-up": 5, "triangle-up-open": 105,
-            "triangle-up-dot": 205, "triangle-up-open-dot": 305, "triangle-down": 6, "triangle-down-open": 106,
-            "triangle-down-dot": 206, "triangle-down-open-dot": 306, "triangle-left": 7, "triangle-left-open": 107,
-            "triangle-left-dot": 207, "triangle-left-open-dot": 307, "triangle-right": 8, "triangle-right-open": 108,
-            "triangle-right-dot": 208, "triangle-right-open-dot": 308, "triangle-ne": 9, "triangle-ne-open": 109,
-            "triangle-ne-dot": 209, "triangle-ne-open-dot": 309, "triangle-se": 10, "triangle-se-open": 110,
-            "triangle-se-dot": 210, "triangle-se-open-dot": 310, "triangle-sw": 11, "triangle-sw-open": 111,
-            "triangle-sw-dot": 211, "triangle-sw-open-dot": 311, "triangle-nw": 12, "triangle-nw-open": 112,
-            "triangle-nw-dot": 212, "triangle-nw-open-dot": 312, "pentagon": 13, "pentagon-open": 113,
-            "pentagon-dot": 213, "pentagon-open-dot": 313, "hexagon": 14, "hexagon-open": 114, "hexagon-dot": 214,
-            "hexagon-open-dot": 314, "hexagon2": 15, "hexagon2-open": 115, "hexagon2-dot": 215,
+            "circle": 0, "circle-open": 100, "circle-dot": 200, "circle-open-dot": 300, "square": 1,
+            "square-open": 101, "square-dot": 201, "square-open-dot": 301, "diamond": 2, "diamond-open": 102,
+            "diamond-dot": 202, "diamond-open-dot": 302, "cross": 3, "cross-open": 103, "cross-dot": 203,
+            "cross-open-dot": 303, "x": 4, "x-open": 104, "x-dot": 204, "x-open-dot": 304, "triangle-up": 5,
+            "triangle-up-open": 105, "triangle-up-dot": 205, "triangle-up-open-dot": 305, "triangle-down": 6,
+            "triangle-down-open": 106, "triangle-down-dot": 206, "triangle-down-open-dot": 306, "triangle-left": 7,
+            "triangle-left-open": 107, "triangle-left-dot": 207, "triangle-left-open-dot": 307, "triangle-right": 8,
+            "triangle-right-open": 108, "triangle-right-dot": 208, "triangle-right-open-dot": 308, "triangle-ne": 9,
+            "triangle-ne-open": 109, "triangle-ne-dot": 209, "triangle-ne-open-dot": 309, "triangle-se": 10,
+            "triangle-se-open": 110, "triangle-se-dot": 210, "triangle-se-open-dot": 310, "triangle-sw": 11,
+            "triangle-sw-open": 111, "triangle-sw-dot": 211, "triangle-sw-open-dot": 311, "triangle-nw": 12,
+            "triangle-nw-open": 112, "triangle-nw-dot": 212, "triangle-nw-open-dot": 312, "pentagon": 13,
+            "pentagon-open": 113, "pentagon-dot": 213, "pentagon-open-dot": 313, "hexagon": 14, "hexagon-open": 114,
+            "hexagon-dot": 214, "hexagon-open-dot": 314, "hexagon2": 15, "hexagon2-open": 115, "hexagon2-dot": 215,
             "hexagon2-open-dot": 315, "octagon": 16, "octagon-open": 116, "octagon-dot": 216, "octagon-open-dot": 316,
             "star": 17, "star-open": 117, "star-dot": 217, "star-open-dot": 317, "hexagram": 18, "hexagram-open": 118,
             "hexagram-dot": 218, "hexagram-open-dot": 318, "star-triangle-up": 19, "star-triangle-up-open": 119,
@@ -146,3 +148,80 @@ class TestGroupScatterGraph(TestCase):
             162: 0  # -> repeating symbols because more groups than available symbols
             }
         np.testing.assert_array_equal(self.ins._symbol_scheme(tested_object), expected_object)
+
+    def test_symbol_scheme_nothingtodo(self):
+        tested_object = ["A", "B", "C"]
+        tested_symbols = {"A": 0, "B": 1, "C": 101}
+        expected_object = {"A": 0, "B": 1, "C": 101}
+        np.testing.assert_array_equal(self.ins._symbol_scheme(tested_object, tested_symbols), expected_object)
+
+
+class TestGroupScatter3DGraph(TestCase):
+
+    def setUp(self):
+        self.test_df = pd.DataFrame.from_dict({
+            "sample1": [1.23, 4.8, 0.34, "A", "y"],
+            "sample2": [-3.4, 0.87, 2.34, "B", "n"],
+            "sample3": [0.99, -1.89, -3.5, "A", "n"],
+            "sample4": [-2.22, -1.09, -0.019, "C", "y"],
+        }, orient="index", columns=["var1", "var2", "var3", "group", "smoking"])
+        self.ins = GroupScatter3DGraph(self.test_df)
+
+    def test_plot_one_graph_1groupcol(self):
+        tested_fig = self.ins.plot_one_graph(
+            "var1", "var2", "var3", "group",
+            show=False, plotting_options={"layout": {"height": 500, "width": 500}}
+        )
+        self.assertEqual(tested_fig["data"][0]["marker"]["color"], "#A63A50")
+        np.testing.assert_array_equal(
+            tested_fig["data"][0]["x"], np.array([1.23, 0.99]))
+        np.testing.assert_array_equal(
+            tested_fig["data"][0]["y"], np.array([4.8, -1.89]))
+        np.testing.assert_array_equal(
+            tested_fig["data"][0]["z"], np.array([0.34, -3.5]))
+        self.assertEqual(tested_fig["data"][0]["name"], "A")
+
+        self.assertEqual(tested_fig["data"][1]["marker"]["color"], "#FFBF00")
+        self.assertEqual(tested_fig["data"][2]["marker"]["color"], "#68ace8")
+
+    def test_plot_one_graph_2groupcol(self):
+        tested_fig = self.ins.plot_one_graph(
+            "var1", "var2", "var3", "group",
+            group_col2="smoking", show=False
+        )
+        self.assertEqual(tested_fig["data"][0]["marker"], go.scatter3d.Marker({'color': '#A63A50', 'symbol': 'circle'}))
+        self.assertEqual(tested_fig["data"][1]["marker"], go.scatter3d.Marker({'color': '#68ace8', 'symbol': 'circle'}))
+        self.assertEqual(tested_fig["data"][2]["marker"], go.scatter3d.Marker({'color': '#A63A50', 'symbol': 'cross'}))
+        self.assertEqual(tested_fig["data"][3]["marker"], go.scatter3d.Marker({'color': '#FFBF00', 'symbol': 'cross'}))
+
+    def test_symbol_scheme3d_under8_woforcedsymbols(self):
+        tested_object = ["A", "B", "C"]
+        expected_object = {"A": "circle", "B": "cross", "C": "diamond"}
+        np.testing.assert_array_equal(self.ins._symbol_scheme3d(tested_object), expected_object)
+
+    def test_symbol_scheme3d_under8_wforcedsymbols(self):
+        tested_object = ["A", "B", "C", "D", "E"]
+        forced_symbols = {
+            "A": "square",
+            "B": "diamond",
+        }
+        expected_object = {
+            "C": "circle", "D": "cross", "E": "circle-open",
+            "A": "square", "B": "diamond",
+            }
+        np.testing.assert_array_equal(
+            self.ins._symbol_scheme3d(tested_object, symbols=forced_symbols),
+            expected_object)
+
+    def test_symbol_scheme3d_over8(self):
+        tested_object = list(range(0, 9))
+        expected_object = {
+            0: 'circle', 1: 'cross', 2: 'diamond', 3: 'square', 4: 'circle-open', 5: 'diamond-open', 6: 'square-open',
+            7: 'x', 8: 'circle'}
+        np.testing.assert_array_equal(self.ins._symbol_scheme3d(tested_object), expected_object)
+
+    def test_symbol_scheme3d_nothingtodo(self):
+        tested_object = ["A", "B", "C"]
+        tested_symbols = {"A": 'circle', "B": 'cross', "C": 'square-open'}
+        expected_object = {"A": 'circle', "B": 'cross', "C": 'square-open'}
+        np.testing.assert_array_equal(self.ins._symbol_scheme3d(tested_object, tested_symbols), expected_object)
